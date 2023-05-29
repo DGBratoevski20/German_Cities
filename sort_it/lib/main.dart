3ebb0d1663bf482114cc7f5abb24238da9b03c92
Denislav Bratoevski <dgbratoevski20@codingburgas.bg>
2023-05-29 22:30:45 +0300

    Edit the whole app

--- sort_it/lib/main.dart
+++ sort_it/lib/main.dart
@@ -15,7 +15,43 @@ class _MyAppState extends State<MyApp> {
   int _selectedIndex = -1;
   bool _showAnimation = false;
 
-  final List<String> _imageUrls = [    "https://picsum.photos/id/1/300/300",    "https://picsum.photos/id/2/300/300",    "https://picsum.photos/id/3/300/300",    "https://picsum.photos/id/4/300/300",    "https://picsum.photos/id/5/300/300",    "https://picsum.photos/id/6/300/300",    "https://picsum.photos/id/7/300/300",    "https://picsum.photos/id/8/300/300",    "https://picsum.photos/id/9/300/300",    "https://picsum.photos/id/10/300/300",    "https://picsum.photos/id/11/300/300",    "https://picsum.photos/id/12/300/300",    "https://picsum.photos/id/13/300/300",    "https://picsum.photos/id/14/300/300",    "https://picsum.photos/id/15/300/300",    "https://picsum.photos/id/16/300/300",  ];
+  final List<String> _imageUrls = [
+    "https://media.cntraveler.com/photos/5b914e80d5806340ca438db1/1:1/w_2250,h_2250,c_limit/BrandenburgGate_2018_GettyImages-549093677.jpg7",
+    "https://a.cdn-hotels.com/gdcs/production19/d1430/c53e41bd-1e9b-4c80-b15b-01e81b1c4679.jpg?impolicy=fcrop&w=800&h=533&q=medium",
+    "https://cdn.britannica.com/06/152206-050-72BD5CAC/twin-towers-Church-of-Our-Lady-Munich.jpg",
+    "https://lp-cms-production.imgix.net/2023-03/cologne-GettyImages-1144623412-rfc.jpeg",
+    "https://media.cnn.com/api/v1/images/stellar/prod/190418141741-01-what-to-see-frankfurt-germany-photos.jpg?q=w_4191,h_2357,x_0,y_0,c_fill/w_1280",
+    "https://en.stuttgart.de/medien/welcomeImages/GettyImages-c-nullplus-478297438-bearbeitet.jpg.scaled/1062a1f14774857c3ccac80e8f83f2f7.jpg",
+    "https://a.cdn-hotels.com/gdcs/production61/d1121/6dfd3cfe-d31b-4514-b3af-aee8536223d1.jpg",
+    "https://content.r9cdn.net/rimg/dimg/38/61/4f479618-city-32256-172608b0525.jpg?width=1200&height=630&xhint=1813&yhint=1557&crop=true",
+    "https://media.essen.de/media/emg_1/essen_tourismus/bilder_neu_1/tourismus_4/erleben_2/DasistEssen_Skyline_peter_prengel_1920_1zu2.jpg",
+    "https://images.prismic.io/indiecampers-demo/f8302bef-06d7-4063-8d8f-be4359674750_final+leipzig.jpg?auto=compress,format&rect=0,0,1996,1331&w=2000&h=1334",
+    "https://www.bremen-convention.de/_Resources/Persistent/e75135ad3b5970303bb1743f2040bd4d561f8c23/RS53013_200423_026-943x628.jpg",
+    "https://www.dresden.de/media/tourismus/sehenswuerdigkeiten/493x330_panorama_dresden_altstadt.jpg.scaled/944272df651acea2572d1e35d36caaf3.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Hannover_Blick_Neues_Rathaus_01.jpg/1200px-Hannover_Blick_Neues_Rathaus_01.jpg",
+    "https://www.germany.travel/media/redaktion/content/staedte/nuernberg/Nuernberg_Blick_auf_den_Tiergaertnertorplatz.jpg",
+    "https://image.jimcdn.com/app/cms/image/transf/dimension=4096x4096:format=jpg/path/sa6549607c78f5c11/image/idf6d09279ea1b3ad/version/1471191639/image.jpg",
+    "https://content.r9cdn.net/rimg/dimg/08/8c/98d64b51-city-9410-1693517046b.jpg?width=1366&height=768&xhint=3561&yhint=2751&crop=true",
+  ];
+
+  final List<String> _imageNames = [
+    "Berlin",
+    "Hamburg",
+    "Munich",
+    "Cologne",
+    "Frankfurt",
+    "Stuttgart",
+    "Düsseldorf",
+    "Dortmund",
+    "Essen",
+    "Leipzig",
+    "Bremen",
+    "Dresden",
+    "Hanover",
+    "Nuremberg",
+    "Duisburg",
+    "Bochum",
+  ];
 
   void _onImageSelected(int index) {
     setState(() {
@@ -25,17 +61,19 @@ class _MyAppState extends State<MyApp> {
 
   void _onAnimationButtonPressed() {
     setState(() {
-      _showAnimation = true;
+      _showAnimation = !_showAnimation; // Toggle the animation state
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Image Selection Animation Demo',
+      title: 'German cities',
+      debugShowCheckedModeBanner: false, // Remove debug banner
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Image Selection Animation Demo'),
+          title: Text('German cities'),
+          centerTitle: true,
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -60,6 +98,17 @@ class _MyAppState extends State<MyApp> {
                             ? Border.all(color: Colors.blue, width: 4)
                             : null,
                       ),
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Text(
+                          _imageNames[index],
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
                     ),
                   );
                 },
@@ -67,22 +116,59 @@ class _MyAppState extends State<MyApp> {
               ),
             ),
             ElevatedButton(
-              onPressed: _selectedIndex != -1 ? _onAnimationButtonPressed : null,
-              child: Text('Animate Image'),
+              onPressed: () {
+                if (_selectedIndex >= 0) {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('Selected City'),
+                        content: Text(_imageNames[_selectedIndex]),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('OK'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                } else {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('Error'),
+                        content: Text('No city selected.'),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('OK'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }
+              },
+              child: Text('Select'),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 10),
             _showAnimation
                 ? Expanded(
-                    child: Center(
-                      child: AnimatedContainer(
-                        duration: Duration(seconds: 2),
-                        curve: Curves.easeInOut,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          image: DecorationImage(
-                            image: NetworkImage(_imageUrls[_selectedIndex]),
-                            fit: BoxFit.cover,
-                          ),
+                    child: Container(
+                      color: Colors.red, // Set the background color to blue
+                      alignment: Alignment.center,
+                      child: Text(
+                        _selectedIndex >= 0 ? _imageNames[_selectedIndex] : '',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
